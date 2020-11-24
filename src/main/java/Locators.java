5032e82a1534be613d44c9d0c63166d7690c3556
hugewave1 <aigerima.imanbayeva@gmail.com>
2020-11-23 20:57:46 -0800

    Added more test cases and structured my codes

---
+++ src/main/java/Locators.java
@@ -0,0 +1,11 @@
+import org.openqa.selenium.By;
+
+public class Locators {
+    //Media page
+    public static final By LINK_MEDIA = By.xpath("//a[@href='https://romanceabroad.com/media/index']");
+    //Registration
+    public static final By LINK_REGISTRATION= By.xpath("//button[@id='show-registration-block']");
+    //Media search
+    public static final By LINK_SEARCH = By.xpath("//a[@href='https://romanceabroad.com/users/search']");
+
+}
